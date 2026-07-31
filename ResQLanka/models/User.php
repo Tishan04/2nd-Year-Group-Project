68d4336c042a2b6dc74924119c1b98953960f8ee
Tishan Abeydeera <161706818+Tishan04@users.noreply.github.com>
2026-07-31 23:51:15 +0530

    Update User.php

--- ResQLanka/models/User.php
+++ ResQLanka/models/User.php
@@ -1,29 +1,125 @@
 <?php
 
-require_once("../config/database.php");
+require_once "../config/Database.php";
 
-class User{
-
+class User
+{
     private $conn;
+    private $table = "users";
 
-    public function __construct($db){
-
-        $this->conn = $db;
-
+    public function __construct()
+    {
+        $database = new Database();
+        $this->conn = $database->connect();
     }
 
-    public function login($username){
+    /*
+    ---------------------------------
+    LOGIN
+    ---------------------------------
+    */
 
-        $sql = "SELECT * FROM users WHERE username = ?";
+    public function login($username)
+    {
+        $query = "SELECT * FROM users
+                  WHERE username = :username
+                  LIMIT 1";
 
-        $stmt = $this->conn->prepare($sql);
+        $stmt = $this->conn->prepare($query);
 
-        $stmt->execute([$username]);
+        $stmt->bindParam(":username",$username);
+
+        $stmt->execute();
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 
+    /*
+    ---------------------------------
+    CHECK USERNAME
+    ---------------------------------
+    */
+
+    public function usernameExists($username)
+    {
+        $query = "SELECT user_id
+                  FROM users
+                  WHERE username=:username";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":username",$username);
+
+        $stmt->execute();
+
+        return $stmt->rowCount()>0;
+    }
+
+    /*
+    ---------------------------------
+    CHECK EMAIL
+    ---------------------------------
+    */
+
+    public function emailExists($email)
+    {
+        $query = "SELECT user_id
+                  FROM users
+                  WHERE email=:email";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":email",$email);
+
+        $stmt->execute();
+
+        return $stmt->rowCount()>0;
+    }
+
+    /*
+    ---------------------------------
+    REGISTER USER
+    ---------------------------------
+    */
+
+    public function register($data)
+    {
+        $query = "INSERT INTO users(
+
+                    first_name,
+                    last_name,
+                    username,
+                    email,
+                    password,
+                    phone,
+                    address,
+                    district,
+                    role,
+                    tier,
+                    points
+
+                )
+
+                VALUES(
+
+                    :first_name,
+                    :last_name,
+                    :username,
+                    :email,
+                    :password,
+                    :phone,
+                    :address,
+                    :district,
+                    'registered_user',
+                    'Bronze',
+                    0
+
+                )";
+
+        $stmt = $this->conn->prepare($query);
+
+        return $stmt->execute($data);
     }
 
 }
-
 ?>
