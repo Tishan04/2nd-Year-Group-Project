60ab5ece2c00722da45ba2a539ba8cb1aad6ba70
Tishan Abeydeera <161706818+Tishan04@users.noreply.github.com>
2026-08-03 23:59:16 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- ResQLanka/views/auth/register.php
+++ ResQLanka/views/auth/register.php
@@ -26,10 +26,7 @@ function registerSelected($field, $value, $old)
 <head>
     <meta charset="UTF-8">
 
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-    >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>ResQ Lanka - Register</title>
 
