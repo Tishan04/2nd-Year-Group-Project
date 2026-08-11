de6ae5543dac6be1becbb76b70139a9a2b82d94c
Tishan Abeydeera <161706818+Tishan04@users.noreply.github.com>
2026-08-12 00:00:09 +0530

    Update login.php

--- ResQLanka/views/auth/login.php
+++ ResQLanka/views/auth/login.php
@@ -5,7 +5,7 @@ require_once("../../config/session.php");
 <html lang="en">
 
 <head>
-    <meta charset="UTF-7">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ResQ Lanka Login</title>
 
